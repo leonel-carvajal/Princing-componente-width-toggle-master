42e42538a4de57d5da571d0c22b5ac91f62ab995
leonel-carvajal <liosoft777@gmail.com>
2020-11-23 10:56:51 -0600

    add function arrows

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const toggle = document.getElementById('toggle')
 const price = document.querySelectorAll('h3')
+const month = document.getElementById('month')
+const anually = document.getElementById('anually')
 
 toggle.addEventListener('click', (e) => {
   if (toggle.childNodes[1].classList.contains('ball') && !(toggle.childNodes[1].classList.contains('active'))) {
@@ -19,4 +21,20 @@ toggle.addEventListener('click', (e) => {
 
   }
 })
-console.log(price[0].textContent)
+
+window.addEventListener('keydown', (e) => {
+  let k = e.key
+  console.log(k)
+  if (k === 'ArrowRight') { 
+    toggle.childNodes[1].classList.add('active')
+    price[0].textContent = '$19.99'
+    price[1].textContent = '$24.99'
+    price[2].textContent = '$39.99'
+  } 
+  if (k === 'ArrowLeft') {
+    toggle.childNodes[1].classList.remove('active')
+    price[0].textContent = '$199.99'
+    price[1].textContent = '$249.99'
+    price[2].textContent = '$399.99'
+  }
+})
